b4ea6d71b4118921964bea423d1cc095c2fa776a
Kurt Montanaro <kurt.montanaro.a100362@mcast.edu.mt>
2017-05-28 04:24:24 +0200

    arrange blob class initialization

--- MvcApplication1/Controllers/ArticleController.cs
+++ MvcApplication1/Controllers/ArticleController.cs
@@ -64,12 +64,12 @@ namespace MvcApplication1.Controllers
                         string path = Path.Combine(Server.MapPath("~\\Images\\"), image);
                        
                             file.SaveAs(path);
-                            using (MemoryStream mem = new MemoryStream())
-                            {
-                                file.InputStream.CopyTo(mem);
-                                byte[] byteArr = mem.GetBuffer();
-                            }
-                            BlobManager.Instance.UploadBlob(image, file);
+//                             using (MemoryStream mem = new MemoryStream())
+//                             {
+//                                 file.InputStream.CopyTo(mem);
+//                                 byte[] byteArr = mem.GetBuffer();
+//                             }
+//                             BlobManager.Instance.UploadBlob(image, file);
                         Article a = new Article();
                         a.Heading = u.header;
                         a.Sub = u.subheader;
